01873f74ad6edecb0532eaebe56a84460e6dabb3
Bhupendra Singh <145081625+bhupendrar1@users.noreply.github.com>
2025-07-03 23:53:14 +0530

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,106 @@
+import random
+
+# List of words to choose from
+word_list = ['python', 'hangman', 'challenge', 'programming', 'developer']
+
+# Choose a random word from the list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+# Display settings
+display = ['_'] * word_length
+lives = 6
+guessed_letters = []
+
+# Hangman stages (for visual effect)
+stages = [
+    '''
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     / \\  |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     /    |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|\\  |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    ''', '''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    '''
+]
+
+print("Welcome to Hangman!")
+
+# Game loop
+while lives > 0 and '_' in display:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in guessed_letters:
+        print(f"You already guessed '{guess}'")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess in chosen_word:
+        for index in range(word_length):
+            if chosen_word[index] == guess:
+                display[index] = guess
+        print("Correct guess!")
+    else:
+        lives -= 1
+        print(f"Wrong guess! You lost a life. Lives remaining: {lives}")
+
+    print(stages[6 - lives])
+    print("Word: ", ' '.join(display))
+    print("Guessed letters:", ', '.join(guessed_letters))
+
+# Game result
+if '_' not in display:
+    print("🎉 Congratulations! You guessed the word correctly!")
+else:
+    print(f"💀 Game Over! The word was '{chosen_word}'. Better luck next time.")
+
